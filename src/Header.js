3a89ae6be1b49cb09c5f6c208b9cfc6d2549efc7
ramamike <7629756@gmail.com>
2022-06-07 06:40:07 +0300

    import-export

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <a href="#s">Home</a>
+            <a href="#s">Messages</a>
+        </div>
+    );
+}
+export default Header;
---
+++ src/Technologies.js
@@ -0,0 +1,12 @@
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>hyml</li>
+                <li>js</li>
+            </ul>
+        </div>
+    );
+}
+export default Technologies;
